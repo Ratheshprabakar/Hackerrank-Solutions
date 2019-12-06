407a2212070cd82afb6c0d3802477c361533f089
Rathesh <ratheshprabakar@gmail.com>
2019-12-06 13:23:29 +0530

    Create Staircase.c

---
+++ Staircase.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,k;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+        for(k=i;k>0;k--)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
